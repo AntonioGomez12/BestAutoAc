6932a209da08fd3cbb9d5fb16d9fc81e9927261e
antonio <gomezant001@gmail.com>
2023-07-23 20:54:58 -0700

    added image v-window

---
+++ bestAutoAc/src/components/ImageViewer.vue
@@ -0,0 +1,157 @@
+<template>
+    <div class="image-viewer">
+      <!-- Image container with navigation buttons -->
+      <div class="image-container">
+        <!-- Navigation buttons -->
+        <v-btn
+          variant="icon"
+          :disabled="currentIndex === 0"
+          @click="prev"
+          class="nav-button left"
+        >
+          <span class="mdi mdi-chevron-left"></span>
+        </v-btn>
+        <v-btn
+          variant="icon"
+          :disabled="currentIndex === images.length - 1"
+          @click="next"
+          class="nav-button right"
+        >
+          <span class="mdi mdi-chevron-right"></span>
+        </v-btn>
+  
+        <!-- Image -->
+        <img :src="currentImage" alt="Image" class="image" />
+  
+        <!-- Indicators (small circles) -->
+        <v-card-actions class="indicators-container">
+          <v-btn
+            v-for="n in images.length"
+            :key="`indicator-${n}`"
+            :variant="currentIndex === n - 1 ? 'text' : 'outlined'"
+            icon
+            @click="goToImage(n - 1)"
+            class="indicator"
+          >
+            <span class="mdi mdi-record"></span>
+          </v-btn>
+        </v-card-actions>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  // Import your images or use the appropriate paths
+  import ac1 from "@/assets/ac1.jpg";
+  import ac2 from "@/assets/ac2.jpg";
+  import ac3 from "@/assets/ac3.jpg";
+  
+  export default {
+    name: "ImageViewer",
+    data() {
+      return {
+        images: [ac1, ac2, ac3], // Replace with your imported images or image paths
+        currentIndex: 0,
+        autoSwitchInterval: null,
+      };
+    },
+    computed: {
+      currentImage() {
+        return this.images[this.currentIndex];
+      },
+      activeIndicator() {
+        return this.currentIndex + 1;
+      },
+    },
+    methods: {
+      prev() {
+        this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+      },
+      next() {
+        this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      },
+      goToImage(index) {
+        this.currentIndex = index;
+      },
+      startAutoSwitch() {
+        this.autoSwitchInterval = setInterval(() => {
+          this.next();
+        }, 10000); // 10 seconds interval
+      },
+      stopAutoSwitch() {
+        clearInterval(this.autoSwitchInterval);
+      },
+    },
+    mounted() {
+      // Start auto-switching on component mount
+      this.startAutoSwitch();
+    },
+    beforeDestroy() {
+      // Stop auto-switching on component destroy
+      this.stopAutoSwitch();
+    },
+  };
+  </script>
+  
+  <style>
+  /* Add your CSS styles for the image viewer here */
+  .image-viewer {
+    display: flex;
+    position: fixed;
+    top: 0;
+    left: 0;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+  }
+  
+  .image-container {
+    position: relative;
+    width: 100%;
+    height: 60%;
+  }
+  
+  .image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  
+  .nav-button {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    color: #fff;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1;
+  }
+  
+  .nav-button.left {
+    left: 0;
+  }
+  
+  .nav-button.right {
+    right: 0;
+  }
+  
+  .indicators-container {
+    position: absolute;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  
+  .indicator {
+    margin: 0 5px;
+  }
+  
+  .v-btn--outlined:not(.v-btn--disabled) .mdi {
+    color: #fff;
+  }
+  
+  .v-btn--text:not(.v-btn--disabled) .mdi {
+    color: #000;
+  }
+  </style>
+  
---
+++ bestAutoAc/src/components/NavBar.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav class="navbar">
+      <ul class="nav-links">
+        <!-- Replace the links below with the appropriate routes or URLs -->
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NavigationBar',
+  };
+  </script>
+  
+  <style>
+  /* Add your CSS styles for the navigation bar here */
+  .navbar {
+    display: flex;
+    width: 100%;
+    height: 20%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color: #333;
+    align-items: flex-end;
+    color: #fff;
+  }
+  
+  .nav-links {
+    position: bottom;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  
+  .nav-links li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  
+  .nav-links li a {
+    color: #fff;
+    text-decoration: none;
+  }
+  
+  .nav-links li a:hover {
+    color: #ddd;
+  }
+  </style>
+  
